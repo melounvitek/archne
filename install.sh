256084eefe7e805278a01fcc57da07aefd2a8197
Vít Meloun <vitek@meloun.info>
2025-07-18 19:02:24 +0200

    Chromium for webapps
    Usage:
    
    ```
    chromium --ozone-platform=wayland --app=https://messenger.com
    ```

--- install.sh
+++ install.sh
@@ -14,7 +14,8 @@ $SUDO pacman -Syu --needed \
   grim slurp wl-clipboard brightnessctl playerctl \
   swaylock xdg-desktop-portal-hyprland \
   network-manager-applet firefox pipewire-pulse \
-  git base-devel zsh dunst pavucontrol swappy fd libnotify tmux
+  git base-devel zsh dunst pavucontrol swappy fd libnotify \
+  tmux chromium
 
 install_dir() { mkdir -p "$HOME/.config/$1"; }
 
