c3d08412a5c7b8b1fa8eb17a004d781fc87258d5
Vitek <vitek@meloun.info>
2025-08-16 14:28:19 +0200

    Install my webapps

--- omarchy_install.sh
+++ omarchy_install.sh
@@ -9,26 +9,34 @@ GITHUB_RAW="https://raw.githubusercontent.com/melounvitek/archne/main/config"
 ((EUID != 0)) && SUDO=sudo || SUDO=
 
 echo "Updating & installing core packages…"
-$SUDO pacman -Syu --needed less vim zsh syncthing htop
+$SUDO pacman -Syu --needed less vim zsh syncthing htop tree
 $SUDO yay -S --needed google-chrome
 
 fetch_or_copy() {
-  sub=$1
-  file=$2
-  src="./config/$sub/$file"
-  dst="$HOME/.config/$sub/$file"
-  if [[ -f "$src" ]]; then
-    cp "$src" "$dst"
+  src_file=$1
+  dst="$HOME/.$src_file"
+  if [[ -f "$src_file" ]]; then
+    cp "$src_file" "$dst"
   else
-    curl -fsSL "$GITHUB_RAW/$sub/$file" -o "$dst"
+    curl -fsSL "$GITHUB_RAW/$src_file" -o "$dst"
   fi
-  echo " → $sub/$file"
+  echo " → $src_file"
 }
 
 echo
 echo "Setting up configs…"
-fetch_or_copy hypr archne.conf
-fetch_or_copy nvim/lua/config options.lua
+fetch_or_copy config/hypr/archne.conf
+fetch_or_copy config/nvim/lua/config/options.lua
+
+echo
+echo "Copying web applications…"
+fetch_or_copy local/share/applications/Calendar.desktop
+fetch_or_copy local/share/applications/icons/Calendar.png
+fetch_or_copy local/share/applications/Freelo.desktop
+fetch_or_copy local/share/applications/icons/Freelo.png
+fetch_or_copy local/share/applications/Messenger.desktop
+fetch_or_copy local/share/applications/icons/Messenger.png
+echo
 
 HYPR_CFG="$HOME/.config/hypr/hyprland.conf"
 LINE='source = ~/.config/hypr/archne.conf'
@@ -39,6 +47,7 @@ fi
 
 # ─ Zsh + Oh My Zsh ───────────────────────────
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+  echo
   echo "Installing Oh My Zsh…"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
@@ -53,15 +62,18 @@ echo "Added aliases to $ZSHRC"
 
 grep -qxF 'eval "$(mise activate zsh)"' "$ZSHRC" || echo 'eval "$(mise activate zsh)"' >>"$ZSHRC"
 echo "Mise activated in $ZSHRC"
+echo
 
 echo "Changing default shell to zsh…"
 $SUDO chsh -s "$(command -v zsh)" $(whoami)
+echo
 
 git config --global --replace-all core.pager "less -F -X"
 git config --global core.editor "vim"
 
 echo "Enabling Syncthing (user service)…"
 systemctl --user enable --now syncthing.service
+echo
 
 echo "Preparing Walker patch to open Chromium webapps in Chrome.."
 mkdir -p ~/bin && ln -sf /usr/bin/google-chrome-stable ~/bin/chromium
