22f1554321707e52d0afebf59ca33d3b864e5a08
Vítek <vitek@meloun.info>
2025-09-11 08:20:27 +0200

    Fix Slack installation

--- omarchy_install.sh
+++ omarchy_install.sh
@@ -7,8 +7,8 @@ GITHUB_REPO="https://raw.githubusercontent.com/melounvitek/archne/main/"
 
 echo
 echo "Updating & installing core packages…"
-$SUDO pacman -Syu --needed --noconfirm less vim zsh syncthing htop tree transmission-gtk zoxide slack-desktop
-yay -S --needed --noconfirm google-chrome ookla-speedtest-bin
+$SUDO pacman -Syu --needed --noconfirm less vim zsh syncthing htop tree transmission-gtk zoxide
+yay -S --needed --noconfirm google-chrome ookla-speedtest-bin slack-desktop
 
 fetch_or_copy() {
   src_file=$1
