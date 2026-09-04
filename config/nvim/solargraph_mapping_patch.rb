87da3e05187eef725a69992b4ce8693679a8e883
Vítek Meloun <vitek@meloun.info>
2026-09-04 13:35:01 +0200

    Fix Solargraph 0.60.4 workspace mapping

--- config/nvim/solargraph_mapping_patch.rb
+++ config/nvim/solargraph_mapping_patch.rb
@@ -1,10 +1,11 @@
 require "solargraph"
 
-# Solargraph 0.60.3 maps files concurrently with requests that iterate the same hash.
+# Solargraph 0.60.3 and 0.60.4 map files concurrently with requests that iterate the same hash.
 module SolargraphMappingPatch
   def next_map
     mutex.synchronize { super }
   end
 end
 
-Solargraph::Library.prepend(SolargraphMappingPatch) if Solargraph::VERSION == "0.60.3"
+affected_versions = %w[0.60.3 0.60.4]
+Solargraph::Library.prepend(SolargraphMappingPatch) if affected_versions.include?(Solargraph::VERSION)
